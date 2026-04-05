1101122bbebea6eff88e3c01e16f141eb5382fc2
Alegodix <alexandre.godineau@etu.cyu.fr>
2026-04-06 00:08:32 +0200

    Ajout mineur meilleure syntaxe

--- Includes/header.php
+++ Includes/header.php
@@ -12,7 +12,7 @@
         <nav id="menu_principal">
             <ul>
                 <li><a href="accueil.php">Accueil</a></li>
-                <li><a href="presentation.php">Presentation</a></li>
+                <li><a href="presentation.php">Présentation</a></li>
                 <li><a href="administrateur.php">Administrateur</a></li>
                 <li><a href="connexion.php">Connexion</a></li>
                 <li><a href="inscription.php">Inscription</a></li>
